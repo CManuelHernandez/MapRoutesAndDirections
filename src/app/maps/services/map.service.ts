a694275d086349586d9ba133c06b412f52e7c02a
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-16 12:57:17 +0200

    Obtener la distancia y duración del recorrido

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interfaces';
+import { DirectionsApiClient } from '../api';
+import { DirectionsResponse, Route } from '../interfaces/directions.interface';
 
 @Injectable({ providedIn: 'root' })
 export class MapService {
@@ -11,7 +13,7 @@ export class MapService {
     return !!this.map;
   }
 
-  constructor() {}
+  constructor(private directionsApi: DirectionsApiClient) {}
 
   setMap(map: Map) {
     this.map = map;
@@ -56,6 +58,29 @@ export class MapService {
     bounds.extend(userLocation);
 
     this.map.fitBounds(bounds, {
+      padding: 200,
+    });
+  }
+
+  getRouteBetweenPoints(start: [number, number], end: [number, number]) {
+    this.directionsApi
+      .get<DirectionsResponse>(`/${start.join(',')};${end.join(',')}`)
+      .subscribe((resp) => this.drawPolyline(resp.routes[0]));
+  }
+
+  private drawPolyline(route: Route) {
+    console.log({ kms: route.distance / 1000, duration: route.duration / 60 });
+
+    if (!this.map) throw Error('Mapa no inicializado');
+
+    const coords = route.geometry.coordinates;
+
+    const bounds = new LngLatBounds();
+    coords.forEach(([lng, lat]) => {
+      bounds.extend([lng, lat]);
+    });
+
+    this.map?.fitBounds(bounds, {
       padding: 200,
     });
   }
