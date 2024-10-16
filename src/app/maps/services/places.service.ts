1901b4df113c61fa13743fc66081912a9f7e3b4b
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-16 11:41:54 +0200

    Custom Http Client - PlacesApiClient

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature, PlacesResponse } from '../interfaces/places.interfaces';
+import { PlacesApiClient } from '../api/placesApiClient';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +16,7 @@ export class PlacesService {
     return !!this.userLocation;
   }
 
-  constructor(private http: HttpClient) {
+  constructor(private placesApi: PlacesApiClient) {
     this.getUserLocation();
   }
 
@@ -36,11 +37,16 @@ export class PlacesService {
   }
 
   getPlacesByQuery(query: string = '') {
-    this.http
-      .get<PlacesResponse>(
-        `https://api.mapbox.com/search/geocode/v6/forward?q=${query}&proximity=-3.5331118096246428%2C40.379299755498636&language=es&access_token=pk.eyJ1IjoiY21hbmgiLCJhIjoiY20yMXg2Nnk3MDE1bDJrcXZzcGxjbG92ZyJ9.Trtaa39Q6v9TmL_wDcD9tw`
-      )
+    if (!this.userLocation) throw Error('No hay userLocation');
+
+    this.placesApi
+      .get<PlacesResponse>(`q=${query}`, {
+        params: {
+          proximity: this.userLocation.join(','),
+        },
+      })
       .subscribe((resp) => {
+        console.log(resp.features);
         this.isLoadingPlaces = false;
         this.places = resp.features;
       });
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiKey:
+    'pk.eyJ1IjoiY21hbmgiLCJhIjoiY20yMXg2Nnk3MDE1bDJrcXZzcGxjbG92ZyJ9.Trtaa39Q6v9TmL_wDcD9tw',
+};
