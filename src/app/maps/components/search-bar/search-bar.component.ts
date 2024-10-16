5698f49d0de953eea39003fe90a91ac827bf017d
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-16 10:36:51 +0200

    Debounce Manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+  constructor(private placesService: PlacesService) {}
 
+  private debounceTimer?: NodeJS.Timeout;
+
+  onQueryChanged(query: string = '') {
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+      console.log(query);
+    }, 350);
+  }
 }
