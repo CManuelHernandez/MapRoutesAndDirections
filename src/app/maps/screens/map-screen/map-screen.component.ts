4927ec24067badc84c2552865ebc0b3fbcd24bf2
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-16 09:22:21 +0200

    Mostrar un mensaje de carga

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -8,4 +8,8 @@ import { PlacesService } from '../../services';
 })
 export class MapScreenComponent {
   constructor(private placesService: PlacesService) {}
+
+  get isUserLocationRedy() {
+    return this.placesService.isUserLocationRedy;
+  }
 }
