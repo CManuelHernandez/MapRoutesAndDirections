5c7be559452d11d1e574a84ef36273dc8fd9586f
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-16 12:22:48 +0200

    Agregar marcadores en los lugares encontrados

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map } from 'mapbox-gl';
+import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { Feature } from '../interfaces/places.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class MapService {
   private map?: Map;
+  private markers: Marker[] = [];
 
   get isMapReady() {
     return !!this.map;
   }
+
+  constructor() {}
 
   setMap(map: Map) {
     this.map = map;
@@ -22,5 +26,26 @@ export class MapService {
     });
   }
 
-  constructor() {}
+  createMarkersFromPlaces(places: Feature[]) {
+    if (!this.map) throw Error('Mapa no inicializado');
+
+    this.markers.forEach((marker) => marker.remove());
+    const newMarkers = [];
+
+    for (const place of places) {
+      const [lng, lat] = place.center;
+      const popup = new Popup().setHTML(`
+          <h6>${place.text}</h6>
+          <span>${place.place_name}</span>
+        `);
+
+      const newMarker = new Marker()
+        .setLngLat([lng, lat])
+        .setPopup(popup)
+        .addTo(this.map);
+
+      newMarkers.push(newMarker);
+    }
+    this.markers = newMarkers;
+  }
 }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature, PlacesResponse } from '../interfaces/places.interfaces';
 import { PlacesApiClient } from '../api/placesApiClient';
+import { MapService } from './map.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,10 @@ export class PlacesService {
     return !!this.userLocation;
   }
 
-  constructor(private placesApi: PlacesApiClient) {
+  constructor(
+    private placesApi: PlacesApiClient,
+    private mapService: MapService
+  ) {
     this.getUserLocation();
   }
 
@@ -54,6 +58,8 @@ export class PlacesService {
       .subscribe((resp) => {
         this.isLoadingPlaces = false;
         this.places = resp.features;
+
+        this.mapService.createMarkersFromPlaces(this.places);
       });
   }
 }
